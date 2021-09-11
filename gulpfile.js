fa613a9cd584559a707e9f837d74a87258ba12c1
Olga Chernikova <darkred2694@gmail.com>
2021-09-11 14:36:36 +0300

    Исправляет алфавитный фильтр в каталоге

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,123 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const htmlmin = require("gulp-htmlmin");
-const csso = require("postcss-csso");
-const rename = require("gulp-rename");
-const terser = require("gulp-terser");
-const squoosh = require("gulp-libsquoosh");
-const webp = require("gulp-webp");
-const svgstore = require("gulp-svgstore");
-const del = require("del");
-const sync = require("browser-sync").create();
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const csso = require('postcss-csso');
+const rename = require('gulp-rename');
+const terser = require('gulp-terser');
+const squoosh = require('gulp-libsquoosh');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
+const sync = require('browser-sync').create();
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/style.scss")
+  return gulp
+    .src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer(),
-      csso()
-    ]))
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(postcss([autoprefixer(), csso()]))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('build/css'))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
 // HTML
 
 const html = () => {
-  return gulp.src("source/*.html")
-  .pipe(htmlmin({ collapseWhitespace: true }))
-  .pipe(gulp.dest("build"));
-}
+  return gulp
+    .src('source/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'));
+};
 
 exports.html = html;
 
 // Scripts
 
 const script = () => {
-  return gulp.src("source/js/script.js")
+  return gulp
+  .src('source/js/script.js')
   .pipe(terser())
-  .pipe(rename("script.min.js"))
-  .pipe(gulp.dest("build/js"))
-}
+  .pipe(rename('script.min.js'))
+  .pipe(gulp.dest('build/js'));
+};
 
 exports.script = script;
 
 //Images
 
 const optimizeImages = () => {
-  return gulp.src("source/img/**/*.{svg,jpg,png}")
+  return gulp
+  .src('source/img/**/*.{svg,jpg,png}')
   .pipe(squoosh())
-  .pipe(gulp.dest("build/img"))
-}
+  .pipe(gulp.dest('build/img'));
+};
 
 exports.optimizeImages = optimizeImages;
 
 const copyImages = () => {
-  return gulp.src("source/img/**/*.{svg,jpg,png}")
-  .pipe(gulp.dest("build/img"))
-}
+  return gulp
+  .src('source/img/**/*.{svg,jpg,png}')
+  .pipe(gulp.dest('build/img'));
+};
 
 exports.copyImages = copyImages;
 
 // Webp
 
 const createWebp = () => {
-  return gulp.src("source/img/**/*.{jpg,png}")
-  .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("build/img"))
-}
+  return gulp
+    .src('source/img/**/*.{jpg,png}')
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest('build/img'));
+};
 
 exports.createWebp = createWebp;
 
 // Sprite
 
 const sprite = () => {
-  return gulp.src("source/img/icons/*.svg")
-  .pipe(svgstore({
-    inlineSvg:true
-  }))
-  .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("build/img"))
-}
+  return gulp
+    .src('source/img/icons/*.svg')
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      }),
+    )
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
+};
 
 exports.sprite = sprite;
 
 // Copy
 
 const copy = (done) => {
-  gulp.src([
-    "source/fonts/*.{woff2,woff}",
-    "source/favicon/**/*.*",
-    "source/img/**/*.svg",
-    "!source/img/icons/*.svg"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"))
+  gulp
+    .src(['source/fonts/*.{woff2,woff}', 'source/favicon/**/*.*', 'source/img/**/*.svg', '!source/img/icons/*.svg'], {
+      base: 'source',
+    })
+    .pipe(gulp.dest('build'));
   done();
-}
+};
 
 exports.copy = copy;
 
 // Clean
 
 const clean = () => {
-  return del("build")
-}
+  return del('build');
+};
 
 exports.clean = clean;
 
@@ -124,14 +126,14 @@ exports.clean = clean;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'build'
+      baseDir: 'build',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
@@ -140,30 +142,19 @@ exports.server = server;
 const reload = (done) => {
   sync.reload();
   done();
-}
+};
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("sousce/js/script.js", gulp.series(script, reload));
-  gulp.watch("source/*.html", gulp.series(html, reload));
-}
+  gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
+  gulp.watch('sousce/js/script.js', gulp.series(script, reload));
+  gulp.watch('source/*.html', gulp.series(html, reload));
+};
 
 // Build
 
-const build = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    styles,
-    html,
-    script,
-    sprite,
-    createWebp
-    ),
-  );
+const build = gulp.series(clean, copy, optimizeImages, gulp.parallel(styles, html, script, sprite, createWebp));
 
 exports.build = build;
 
@@ -173,15 +164,6 @@ exports.default = gulp.series(
   clean,
   copy,
   copyImages,
-  gulp.parallel(
-    styles,
-    html,
-    script,
-    sprite,
-    createWebp
-  ),
-  gulp.series(
-    server,
-    watcher
-  )
+  gulp.parallel(styles, html, script, sprite, createWebp),
+  gulp.series(server, watcher),
 );
--- source/js/script.js
+++ source/js/script.js
@@ -1,46 +1,46 @@
-const navMenu = document.querySelector(".header__menu");
-const navToggle = document.querySelector(".header__toggle");
-const mainHeader = document.querySelector(".header");
-const logoImage = document.querySelector(".header__logo");
-const navLink = document.querySelector(".nav__link");
+const navMenu = document.querySelector('.header__menu');
+const navToggle = document.querySelector('.header__toggle');
+const mainHeader = document.querySelector('.header');
+const logoImage = document.querySelector('.header__logo');
+const navLink = document.querySelector('.nav__link');
 
-mainHeader.classList.remove("header--no-js");
+mainHeader.classList.remove('header--no-js');
 
-navToggle.addEventListener("click", (evt) => {
+navToggle.addEventListener('click', (evt) => {
   evt.preventDefault();
-  if (navMenu.classList.contains("header__menu--closed")) {
-    navMenu.classList.remove("header__menu--closed");
-    navToggle.classList.add("header__toggle--closed");
-    logoImage.classList.add("header__logo--opened");
+  if (navMenu.classList.contains('header__menu--closed')) {
+    navMenu.classList.remove('header__menu--closed');
+    navToggle.classList.add('header__toggle--closed');
+    logoImage.classList.add('header__logo--opened');
     navLink.focus();
   } else {
-    navMenu.classList.add("header__menu--closed");
-    navToggle.classList.remove("header__toggle--closed");
-    logoImage.classList.remove("header__logo--opened");
+    navMenu.classList.add('header__menu--closed');
+    navToggle.classList.remove('header__toggle--closed');
+    logoImage.classList.remove('header__logo--opened');
     navToggle.focus();
-  };
+  }
 });
 
-window.addEventListener("keydown", (evt) => {
-  if (evt.key === "Esc" || evt.key === "Escape") {
-    if (!navMenu.classList.contains("header__menu-closed") && navToggle.classList.contains("header__toggle--close")) {
+window.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Esc' || evt.key === 'Escape') {
+    if (!navMenu.classList.contains('header__menu-closed') && navToggle.classList.contains('header__toggle--close')) {
       evt.preventDefault();
-      navMenu.classList.add("header__menu--closed");
-      navToggle.classList.remove("header__toggle--closed");
+      navMenu.classList.add('header__menu--closed');
+      navToggle.classList.remove('header__toggle--closed');
       navToggle.focus();
-    };
-  };
+    }
+  }
 });
 
 // Паддинг при фиксированном меню
 
 const pageBody = document.body;
-const paddingWrapper = document.querySelector(".padding-wrapper");
-const header = document.querySelector(".header");
+const paddingWrapper = document.querySelector('.padding-wrapper');
+const header = document.querySelector('.header');
 
 const setPageWrapperPadding = () => {
   paddingWrapper.style.paddingTop = `${header.getBoundingClientRect().height}px`;
-}
+};
 
 setPageWrapperPadding();
 
@@ -48,57 +48,57 @@ const windowResizeHandler = () => {
   setPageWrapperPadding();
 };
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (pageBody.getBoundingClientRect().top < 0) {
-    header.classList.add("header--scrolled");
+    header.classList.add('header--scrolled');
     return;
-  };
-  header.classList.remove("header--scrolled");
+  }
+  header.classList.remove('header--scrolled');
 });
 
-window.addEventListener("resize", windowResizeHandler);
+window.addEventListener('resize', windowResizeHandler);
 
 //---- Модальное окно на главной странице
 
-const modal = document.querySelector(".modal");
-const openModalLink = document.querySelector(".open-modal-link");
-const closeModalButton = document.querySelector(".modal-close-button");
+const modal = document.querySelector('.modal');
+const openModalLink = document.querySelector('.open-modal-link');
+const closeModalButton = document.querySelector('.modal-close-button');
 
 const initModalTariff = () => {
-  if(!openModalLink) {
+  if (!openModalLink) {
     return;
-  };
+  }
 
   const clearOpenedModal = () => {
-    document.querySelector(".modal.modal--opened").classList.remove("modal--opened");
+    document.querySelector('.modal.modal--opened').classList.remove('modal--opened');
     enableScrolling();
   };
 
-  openModalLink.addEventListener("click", (evt) => {
+  openModalLink.addEventListener('click', (evt) => {
     evt.preventDefault();
-    modal.classList.add("modal--opened");
+    modal.classList.add('modal--opened');
     disableScrolling();
-    document.addEventListener("click", onDocumentClick);
+    document.addEventListener('click', onDocumentClick);
     closeModalButton.focus();
   });
 
   const onDocumentClick = ({target}) => {
-    if (target.closest(".modal-close-button") || target.closest(".for-business__overlay")) {
+    if (target.closest('.modal-close-button') || target.closest('.for-business__overlay')) {
       clearOpenedModal(modal);
-      document.removeEventListener("click", onDocumentClick);
+      document.removeEventListener('click', onDocumentClick);
       openModalLink.focus();
-    };
+    }
   };
 
-  window.addEventListener("keydown", (evt) => {
-    if (evt.key === "Esc" || evt.key === "Escape") {
+  window.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Esc' || evt.key === 'Escape') {
       evt.preventDefault();
       clearOpenedModal(modal);
       openModalLink.focus();
-    };
+    }
   });
 
-  const body = document.querySelector(`body`);
+  const body = document.querySelector('body');
   window.offsetScroll = 0;
 
   const getScrollbarWidth = () => {
@@ -115,16 +115,16 @@ const initModalTariff = () => {
 
   const disableScrolling = () => {
     const scrollWidth = getScrollbarWidth();
-    body.setAttribute(`style`, `padding-right: ` + scrollWidth + `px;`);
-    body.dataset.scrollY = `${getBodyScrollTop()}`;
+    body.setAttribute('style', 'padding-right: ' + scrollWidth + 'px;');
+    body.dataset.scrollY = getBodyScrollTop();
     window.offsetScroll = getBodyScrollTop();
     body.style.top = `-${document.body.dataset.scrollY}px`;
-    body.classList.add(`fixed`);
+    body.classList.add('fixed');
   };
 
-  const enableScrolling = () =>{
-    body.removeAttribute(`style`);
-    body.classList.remove(`fixed`);
+  const enableScrolling = () => {
+    body.removeAttribute('style');
+    body.classList.remove('fixed');
     window.scrollTo(0, +document.body.dataset.scrollY);
     window.offsetScroll = 0;
   };
@@ -134,20 +134,20 @@ initModalTariff();
 
 //---- Форма регистрации на главной странице
 
-const indexForm = document.querySelector(".authorize__form");
-const emailInput = document.querySelector(".authorize__email-input");
+const indexForm = document.querySelector('.authorize__form');
+const emailInput = document.querySelector('.authorize__email-input');
 
 const initIndexForm = () => {
-  if(!indexForm) {
+  if (!indexForm) {
     return;
-  };
+  }
 
-  indexForm.addEventListener("submit", (evt) => {
+  indexForm.addEventListener('submit', (evt) => {
     if (!emailInput.value) {
       evt.preventDefault();
-      emailInput.placeholder = "Введите e-mail";
-      emailInput.classList.add("authorize__email-input--error");
-    };
+      emailInput.placeholder = 'Введите e-mail';
+      emailInput.classList.add('authorize__email-input--error');
+    }
   });
 };
 
@@ -155,23 +155,23 @@ initIndexForm();
 
 //---- Алфавит на странице формы
 
-const alphabetLink = document.querySelector(".step__alphabet-link");
-const alphabetForm = document.querySelector(".step__alphabet-modal");
-const alphabetClose = document.querySelector(".step__close-alphabet-button");
+const alphabetLink = document.querySelector('.step__alphabet-link');
+const alphabetForm = document.querySelector('.step__alphabet-modal');
+const alphabetClose = document.querySelector('.step__close-alphabet-button');
 
 const initAlphabetForm = () => {
   if (!alphabetLink) {
     return;
-  };
+  }
 
-  alphabetLink.addEventListener("click", (evt) => {
+  alphabetLink.addEventListener('click', (evt) => {
     evt.preventDefault();
-    alphabetForm.classList.add("step__alphabet-modal--opened");
+    alphabetForm.classList.add('step__alphabet-modal--opened');
   });
 
-  alphabetClose.addEventListener("click", (evt) => {
+  alphabetClose.addEventListener('click', (evt) => {
     evt.preventDefault();
-    alphabetForm.classList.remove("step__alphabet-modal--opened");
+    alphabetForm.classList.remove('step__alphabet-modal--opened');
   });
 };
 
@@ -180,10 +180,10 @@ initAlphabetForm();
 //---- Карта
 
 const initMap = () => {
-  const MapContainer = document.querySelector("#map");
+  const MapContainer = document.querySelector('#map');
   if (!MapContainer) {
     return;
-  };
+  }
 
   window.ymaps.ready(function () {
     const map = new window.ymaps.Map(document.querySelector('#map'), {
@@ -191,16 +191,12 @@ const initMap = () => {
       controls: [],
       zoom: 16,
     });
-    const myPlaceMark = new window.ymaps.Placemark(
-      [59.938788, 30.323126],
-      null,
-      {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/pin.svg',
-        iconImageSize: [41, 41],
-        iconImageOffset: [-20 / 2, -10 / 2],
-      },
-    );
+    const myPlaceMark = new window.ymaps.Placemark([59.938788, 30.323126], null, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/pin.svg',
+      iconImageSize: [41, 41],
+      iconImageOffset: [-20 / 2, -10 / 2],
+    });
     map.geoObjects.add(myPlaceMark);
   });
 };
@@ -209,58 +205,58 @@ initMap();
 
 //---- Алфавит на странице каталога
 
-const alphabetToggle = document.querySelector(".land-filter__toggle");
-const alphabetCatalog = document.querySelector(".land-filter__container");
-const alphabetCloseButton = document.querySelector(".land-filter__close-button");
+const alphabetToggle = document.querySelector('.land-filter__toggle');
+const alphabetCatalog = document.querySelector('.land-filter__container');
+const alphabetCloseButton = document.querySelector('.land-filter__close-button');
 
 const initAlphabetCatalog = () => {
-  if(!alphabetToggle) {
+  if (!alphabetToggle) {
     return;
-  };
+  }
 
-  alphabetCatalog.classList.remove("land-filter__container--no-js");
+  alphabetCatalog.classList.remove('land-filter__container--no-js');
 
-  alphabetToggle.addEventListener("click", (evt) => {
+  alphabetToggle.addEventListener('click', (evt) => {
     evt.preventDefault();
-    const parent = evt.target.closest(".land-filter__container");
-    parent.classList.toggle("land-filter__container--opened");
-    alphabetToggle.classList.toggle("land-filter__toggle--opened");
+    const parent = evt.target.closest('.land-filter__container');
+    parent.classList.toggle('land-filter__container--opened');
+    alphabetToggle.classList.toggle('land-filter__toggle--opened');
   });
 
-  alphabetCloseButton.addEventListener("click", (evt) => {
+  alphabetCloseButton.addEventListener('click', (evt) => {
     evt.preventDefault();
-    alphabetCatalog.classList.remove("land-filter__container--opened");
-    alphabetToggle.classList.remove("land-filter__toggle--opened")
+    alphabetCatalog.classList.remove('land-filter__container--opened');
+    alphabetToggle.classList.remove('land-filter__toggle--opened');
   });
 };
 
 initAlphabetCatalog();
 
-const accordionToggle = document.querySelectorAll(".companions-filter__toggle");
+const accordionToggle = document.querySelectorAll('.companions-filter__toggle');
 accordionToggle.forEach((toggle) => {
-  toggle.addEventListener("click", (evt) => {
+  toggle.addEventListener('click', (evt) => {
     evt.preventDefault();
-    evt.target.classList.toggle("companions-filter__toggle--opened");
+    evt.target.classList.toggle('companions-filter__toggle--opened');
     const currentList = evt.target.nextElementSibling;
     const currentClass = currentList.classList[0];
     currentList.classList.toggle(`${currentClass}--opened`);
   });
 });
 
-const innerPageForm = document.querySelector(".add-plan__form");
-const commentTextarea = document.querySelector(".step__entertainment-comment");
+const innerPageForm = document.querySelector('.add-plan__form');
+const commentTextarea = document.querySelector('.step__entertainment-comment');
 
 const initInnerPageForm = () => {
-  if(!innerPageForm) {
+  if (!innerPageForm) {
     return;
-  };
+  }
 
-  innerPageForm.addEventListener("submit", (evt) => {
+  innerPageForm.addEventListener('submit', (evt) => {
     if (!commentTextarea.value) {
       evt.preventDefault();
-      commentTextarea.classList.add("step__entertainment-comment--error");
+      commentTextarea.classList.add('step__entertainment-comment--error');
       commentTextarea.focus();
-    };
+    }
   });
 };
 
